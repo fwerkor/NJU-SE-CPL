886ea133d9d974d53958cd63c0ae8ed41a1e925a
fwerkor <3116935098@qq.com>
2025-10-21 09:22:47 +0000

    Upload 35

--- 35/quick-sort.c
+++ 35/quick-sort.c
@@ -4,43 +4,45 @@
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdint.h>
 
 void swap(int *a, int *b) {
-    int t = *a;
+    int temp = *a;
     *a = *b;
-    *b = t;
+    *b = temp;
 }
 
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
+    k--; 
     int a[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-
-    int pivot = a[k - 1];
+    int pivot = a[k];
     int l = 0, r = n - 1;
     
-    swap(&a[k - 1], &a[n - 1]);
-    r = n - 2; 
-    
-    while (l <= r) {
-        while (l <= r && a[l] < pivot) l++;
-        while (l <= r && a[r] > pivot) r--;
-        if (l <= r) {
-            swap(&a[l], &a[r]);
+    while (l < r) {
+        while (l < r && a[l] < pivot) {
             l++;
+        }
+        while (l < r && a[r] >= pivot) {
             r--;
         }
+        if (l < r) {
+            swap(&a[l], &a[r]);
+        }
     }
-    
-    swap(&a[l], &a[n - 1]);
+    swap(&a[l], &a[k]);
     
     for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
-
+    
     return 0;
 }
